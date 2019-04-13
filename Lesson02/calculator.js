1ef37fc6e6cfe7dfa86a445d1daadd99a80384da
georgeme <georgeme@uw.edu>
2019-04-13 14:14:00 -0700

    Adding math operations

--- Lesson02/calculator.js
+++ Lesson02/calculator.js
@@ -1,12 +1,55 @@
+/* calculator.js code */
+/* Declaring variables */
 const v = 64;
 const w = 32;
 const x = 16;
 const y = 4;
 const z = 2;
 
+/* Writing to the console log */
 console.log(4 * (w / 4) + 45);
 console.log((y + 17) / 7);
 console.log(w / x - y);
 console.log(w * (w / z) + y - x);
 console.log((v / y) * (x * z) + 15 % 2);
+console.log(39 + z / y * 5);
+
+
+
+
+/* Expected Results:
+console.log(4 * (w / 4) + 45);
+32 / 4 = 8
+8 * 4 = 32
+32 + 45 = 77 is the value in console.log
+
+console.log((y + 17) / 7);
+4 + 17 = 21
+21 / 7 = 3 is the value in console.log
+
+console.log(w / x - y);
+32 / 16 = 2
+2 - 4 = -2 is the value in the console.log
+
+console.log(w * (w / z) + y - x);
+32 / 2 = 16
+32 * 16 = 512
+512 + 4 = 516
+516 - 16 = 500 is the value in the console log
+
+console.log((v / y) * (x * z) + 15 % 2);
+64 / 4 = 16
+16 (as x) * 2 = 32
+16 (from first equation) * 32 = 512
+15 % 2 = 1 because there is 1 remainder
+512 + 1 remainder = 513 is the value in the console.log
+
+console.log(39 + z / y * 5);
+2 / 4 = .5
+.5 * 5 = 2.5
+39 + 2.5 = 41.5 is the value in the console.log 
+
+*/
+
+
 
