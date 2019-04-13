8f87586b0da0ae7e2f1992ad46f2709f521dc804
georgeme <georgeme@uw.edu>
2019-04-13 13:40:12 -0700

    Adding Lesson02 folder

---
+++ Lesson02/calculator.js
@@ -0,0 +1,12 @@
+const v = 64;
+const w = 32;
+const x = 16;
+const y = 4;
+const z = 2;
+
+console.log(4 * (w / 4) + 45);
+console.log((y + 17) / 7);
+console.log(w / x - y);
+console.log(w * (w / z) + y - x);
+console.log((v / y) * (x * z) + 15 % 2);
+
