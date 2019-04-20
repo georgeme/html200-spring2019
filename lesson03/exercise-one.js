18d39f4bb52edde0b775e3354fe3fa15b6f4c835
georgeme <georgeme@uw.edu>
2019-04-20 13:19:10 -0700

    Added expected results for code

--- lesson03/exercise-one.js
+++ lesson03/exercise-one.js
@@ -6,6 +6,9 @@ greeting.push("salve");
 
 console.log(greeting);
 // end code
+// EXPECTED RESULT:
+// ["hello", "bonjour", "hallo", "hola", "hej", "ciao", "salve"] - added "salve" at the end which is Latin for "hello"
+
 
 
 // code for removing the third item
@@ -15,6 +18,8 @@ greeting.splice(2, 1);
 
 console.log(greeting);
 // end code
+// EXPECTED RESULT:
+// ["hello", "bonjour", "hola", "hej", "ciao"] - removed "hallo" which is the thirde item in the list of greetings
 
 
 // code for creating a single string with comma separated values
@@ -24,6 +29,9 @@ const greetingList=greeting.join(',');
 
 console.log(greetingList);
 // end code
+// EXPECTED RESULT:
+// "hello,bonjour,hallo,hola,hej,ciao" - created as one string with comma separated values
+
 
 
 // code for doing ALL tasks in one result
@@ -37,4 +45,6 @@ const greetingList=greeting.join(',');
 
 console.log(greetingList);
 // end code
+// EXPECTED RESULT: 
+// "hello,bonjour,hola,hej,ciao,salve" - added "salve" to the end of the strings of greetings, removed "hallo" as the third item in the list, wrote them all as one comma separated string
 
