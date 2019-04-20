3c765ca56f4853ece445559488014057c0698b69
georgeme <georgeme@uw.edu>
2019-04-20 14:11:54 -0700

    Corrected values

--- lesson03/exercise-one.js
+++ lesson03/exercise-one.js
@@ -1,29 +1,29 @@
 
 // code for adding an item to the end of the array
-let greeting = ["hello", "bonjour", "hallo", "hola", "hej", "ciao"];
+let greeting = ['hello', 'bonjour', 'hallo', 'hola', 'hej', 'ciao'];
 
-greeting.push("salve");
+greeting.push('salve');
 
 console.log(greeting);
 // end code
 // EXPECTED RESULT:
-// ["hello", "bonjour", "hallo", "hola", "hej", "ciao", "salve"] - added "salve" at the end which is Latin for "hello"
+// ["hello", "bonjour", "hallo", "hola", "hej", "ciao", "salve"] - added 'salve' at the end which is Latin for 'hello'
 
 
 
 // code for removing the third item
-let greeting = ["hello", "bonjour", "hallo", "hola", "hej", "ciao"];
+let greeting = ['hello', 'bonjour', 'hallo', 'hola', 'hej', 'ciao'];
 
 greeting.splice(2, 1);
 
 console.log(greeting);
 // end code
 // EXPECTED RESULT:
-// ["hello", "bonjour", "hola", "hej", "ciao"] - removed "hallo" which is the third item in the list of greetings
+// ["hello", "bonjour", "hola", "hej", "ciao"] - removed 'hallo' which is the third item in the list of greetings
 
 
 // code for creating a single string with comma separated values
-let greeting = ["hello", "bonjour", "hallo", "hola", "hej", "ciao"];
+let greeting = ['hello', 'bonjour', 'hallo', 'hola', 'hej', 'ciao'];
 
 const greetingList=greeting.join(',');
 
@@ -35,9 +35,9 @@ console.log(greetingList);
 
 
 // code for doing ALL tasks in one result
-let greeting = ["hello", "bonjour", "hallo", "hola", "hej", "ciao"];
+let greeting = ['hello', 'bonjour', 'hallo', 'hola', 'hej', 'ciao'];
 
-greeting.push("salve");
+greeting.push('salve');
 
 greeting.splice(2, 1);
 
@@ -46,5 +46,5 @@ const greetingList=greeting.join(',');
 console.log(greetingList);
 // end code
 // EXPECTED RESULT: 
-// "hello,bonjour,hola,hej,ciao,salve" - added "salve" to the end of the strings of greetings, removed "hallo" as the third item in the list, wrote them all as one comma separated string
+// "hello,bonjour,hola,hej,ciao,salve" - added 'salve' to the end of the strings of greetings, removed 'hallo' as the third item in the list, wrote them all as one comma separated string
 
