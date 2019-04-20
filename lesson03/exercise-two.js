ce95b9bb4581a85bfc2ecda766f388cc4db57d0b
georgeme <georgeme@uw.edu>
2019-04-20 14:30:58 -0700

    Adding Assignment 2

---
+++ lesson03/exercise-two.js
@@ -0,0 +1,12 @@
+
+// Creating the sentence of at least five words and a period
+const favMovie = "I love movies and a particular favorite of mine is Strictly Ballroom.";
+
+// Creating the array with splits (separation) at the spaces between words
+const movieArr = favMovie.split(' ');
+
+// Console.log the third word in the array
+console.log(movieArr[2]);
+
+// EXPECTED RESULT:
+// "movies" - this is the third word in the array created by splitting the sentence at the spaces between words
