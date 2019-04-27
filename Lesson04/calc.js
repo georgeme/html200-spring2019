790c0abc337f9d6240c72a6cec340185bdd8845f
georgeme <georgeme@uw.edu>
2019-04-27 11:16:01 -0700

    Updated text in both files

--- Lesson04/calc.js
+++ Lesson04/calc.js
@@ -1,6 +1,6 @@
 // This is the JavaScript for the Regular Challenge homework to square a number entered by a user
 function squareCalc() {
-  let input = prompt('Enter the number to square');
+  let input = prompt('Enter the number to SQUARE');
   let result = 0;
 
   result = Math.pow(input, 2);
@@ -10,8 +10,8 @@ function squareCalc() {
 
 // This is the JavaScript for the Extra Challenge homework to multiply two numbers entered by a user
 function multiplyCalc() {
-  let inputOne = prompt('Enter first number to multiply');
-  let inputTwo = prompt('Enter second number to multiply');
+  let inputOne = prompt('Enter FIRST number to MULTIPLY');
+  let inputTwo = prompt('Enter SECOND number to MULTIPLY');
   let result = 0;
 
   result = (inputOne * inputTwo);
