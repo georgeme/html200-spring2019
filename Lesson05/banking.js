53ff59f73a9db936ccd69213160fba79abb6b522
georgeme <georgeme@uw.edu>
2019-05-04 16:26:08 -0700

    JS including switch statements and extra challenges

--- Lesson05/banking.js
+++ Lesson05/banking.js
@@ -1,4 +1,4 @@
-//
+//wITH IF/ELSE STATEMENTS ** TO BE DELETED**
 // let quit = false;
 // let balance = 50000;
 //
@@ -34,7 +34,50 @@
 //
 // }
 
+//wITH SWITCH STATEMENTS BUT NO NEGATIVE BALANCE CONSEQUENCES ** TO BE DELETED**
+// let quit = false;
+// let balance = 50000;
+//
+// function banking() {
+//   while(quit === false) {
+//   let input = prompt("What transaction would you like?").toLowerCase();
+//
+//     switch(input) {
+//       case 'q':
+//       quit = true;
+//       break;
+//
+//       case 'b':
+//       result = balance;
+//       alert('$' + result.toFixed(2));
+//       break;
+//
+//       case 'd':
+//         let deposit = prompt('How much would you like to deposit?').toLowerCase();
+//         result = Number(balance) + Number(deposit);
+//           if (deposit > 50000) {
+//             alert('You cannot deposit more than $50,000');
+//               { continue; }
+//           }
+//         balance = balance + Number(deposit);
+//         alert('$' + result.toFixed(2));
+//         break;
+//
+//       case 'w':
+//         let withdrawal = prompt("How much would you like to withdraw?").toLowerCase();
+//         result = Number(balance) - Number(withdrawal);
+//         balance = balance - Number(withdrawal);
+//           if (balance < 300) {
+//             alert('Your balance will be less than $300. Do you wish to continue?');
+//           }
+//         alert('$' + result.toFixed(2));
+//         break;
+//     }
+//   }
+// }
 
+
+//wITH SWITCH STATEMENTS AND NEGATIVE BALANCE CONSEQUENCES ** GOOD **
 let quit = false;
 let balance = 50000;
 
@@ -56,7 +99,7 @@ function banking() {
         let deposit = prompt('How much would you like to deposit?').toLowerCase();
         result = Number(balance) + Number(deposit);
           if (deposit > 50000) {
-            alert('You cannot deposit more than $50,000');
+            alert('You cannot deposit more than $50,000!');
               { continue; }
           }
         balance = balance + Number(deposit);
@@ -67,8 +110,13 @@ function banking() {
         let withdrawal = prompt("How much would you like to withdraw?").toLowerCase();
         result = Number(balance) - Number(withdrawal);
         balance = balance - Number(withdrawal);
-          if (balance < 300) {
+          if (balance < 300 && balance > 0) {
             alert('Your balance will be less than $300. Do you wish to continue?');
+          }
+
+          if (balance < 0) {
+            alert('You cannot have a negative balance!');
+              { continue; }
           }
         alert('$' + result.toFixed(2));
         break;
